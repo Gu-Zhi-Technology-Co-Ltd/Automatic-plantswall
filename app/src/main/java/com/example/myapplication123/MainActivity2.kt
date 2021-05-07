2e14bb5b38ebbc2b73a218f5cace37375541e815
jabee0228 <74387204+jabee0228@users.noreply.github.com>
2021-05-07 10:47:54 +0800

    Revert "add button"
    This reverts commit 6b4e96f6

--- app/src/main/java/com/example/myapplication123/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication123/MainActivity2.kt
@@ -2,14 +2,10 @@ package com.example.myapplication123
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-    }
-    fun button(view:View){
-        println("jdi")
     }
 }
