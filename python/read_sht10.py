36e8e3b5b1b208eabc56bad11e995339b9203c17
姜義新 <74387204+jabee0228@users.noreply.github.com>
2021-07-01 15:51:21 +0800

    Update read_sht10.py

--- python/read_sht10.py
+++ python/read_sht10.py
@@ -2,20 +2,17 @@ from time import sleep
 import RPi.GPIO as GPIO
 from pi_sht1x import SHT1x
 
-import time
+
 
 def test_sht1x(DATA_PIN, SCK_PIN):
     with SHT1x(DATA_PIN, SCK_PIN, gpio_mode=GPIO.BOARD) as sensor:
-        for i in range(1):
-            temp = sensor.read_temperature()
-            humidity = sensor.read_humidity(temp)
-            sensor.calculate_dew_point(temp, humidity)
-            print(temp,humidity)
-            return temp, humidity
-            sleep(2)
+        
+        temp = sensor.read_temperature()
+        humidity = sensor.read_humidity(temp)
+        sensor.calculate_dew_point(temp, humidity)
+        print(temp,humidity)
+        return temp, humidity
+        sleep(2)
         print('Test complete.\n')
     
-
-if __name__ == "__main__":
-    main()
 
